9eeab87b3b5f6cb6895ec82cb7eca25b032d83cb
sunmi <dsm851105@gmail.com>
2021-06-24 23:40:27 +0900

    페이지 이동시(unload시) 타이머 상태 저장

--- src/main/java/com/ball/controller/TimerAjaxController.java
+++ src/main/java/com/ball/controller/TimerAjaxController.java
@@ -24,6 +24,31 @@ public class TimerAjaxController {
     @Setter(onMethod_=@Autowired )
     private TimerService timerService;
 
+    @PutMapping(value = "/{timer_id}/{timer_is_play}")
+    public ResponseEntity<String> timerStateUpdate(@PathVariable(value = "timer_id") Long timer_id
+            , @PathVariable(value = "timer_is_play") Integer timer_is_play
+            , @RequestBody String timer_date
+            , HttpSession session){
+        log.info("TimerAjaxController timerStateUpdate........................................");
+        log.info(timer_id+" "+ timer_is_play+" "+ timer_date);
+        try{
+            DateTimeFormatter dtf = DateTimeFormatter.ofPattern("HH:mm:ss");
+            LocalTime timer_time = LocalTime.parse(timer_date, dtf);
+
+            TimerVO vo = new TimerVO();
+            vo.setTimer_id(timer_id);
+            vo.setTimer_accumulated_day(timer_time);
+            vo.setUser_id(String.valueOf(session.getAttribute("userID")));
+            vo.setTimer_is_play(timer_is_play);
+
+            return timerService.modifyTimerAccumulatedDayTimeAndStopState(vo) == 1?
+                    new ResponseEntity<String>("success", HttpStatus.OK)
+                    : new ResponseEntity<String>("db error", HttpStatus.INTERNAL_SERVER_ERROR);
+        }catch (Exception e){
+            return new ResponseEntity<String>("parsing error", HttpStatus.EXPECTATION_FAILED);
+        }
+    }
+
 
     @PutMapping(value = "/putstart/{timer_id}")
     public ResponseEntity<String> timerStartUpdate(@PathVariable(value = "timer_id") Long timer_id
@@ -45,11 +70,13 @@ public class TimerAjaxController {
     public ResponseEntity<String> timerStopUpdate(@PathVariable(value = "timer_id") Long timer_id
             , @RequestBody String timer_date
             , HttpSession session){
-
+        log.info("TimerAjaxController timerStopUpdate........................................");
+        log.info(timer_id+" "+ timer_date);
         try{
             DateTimeFormatter dtf = DateTimeFormatter.ofPattern("HH:mm:ss");
             LocalTime timer_time = LocalTime.parse(timer_date, dtf);
 
+            log.info(timer_id+" "+ timer_date);
             TimerVO vo = new TimerVO();
             vo.setTimer_id(timer_id);
             vo.setTimer_accumulated_day(timer_time);
@@ -60,6 +87,7 @@ public class TimerAjaxController {
                     new ResponseEntity<String>("success", HttpStatus.OK)
                     : new ResponseEntity<String>("db error", HttpStatus.INTERNAL_SERVER_ERROR);
         }catch (Exception e){
+            e.printStackTrace();
             return new ResponseEntity<String>("parsing error", HttpStatus.EXPECTATION_FAILED);
         }
     }
